7fd7d59b1e3c594ae2cdb492f86361519b06df8b
gytar <gytar@glaptop.com>
2024-01-14 15:02:16 +0100

    fix broken about page on mobile + add Louis George Cafe font

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,40 +5,48 @@
         <h2 class="text-5xl font-bold">Qui suis-je ?</h2>
         <div>pic</div>
       </div>
-      <p class="lg:text-xl text-justify mb-2">Développeur web depuis peu, je suis toujours partant pour un nouveau challenge. Je suis fan d’informatique, mais aussi de musique et de chats.</p>
-      <p class="lg:text-xl text-justify">Dans mon travail, je prone des valeurs fortes : l’éducation, l’accessibilité et l’écologie. Il me semble que nous devons toutes et tous faire de notre mieux dans notre domaine en donnant le meilleur de nous même.</p>
+      <p class="lg:text-xl text-justify mb-2">Développeur web depuis peu, je suis toujours partant pour un nouveau
+        challenge. Je suis fan d’informatique, mais aussi de musique et de chats.</p>
+      <p class="lg:text-xl text-justify">Dans mon travail, je prone des valeurs fortes : l’éducation, l’accessibilité et
+        l’écologie. Il me semble que nous devons toutes et tous faire de notre mieux dans notre domaine en donnant le
+        meilleur de nous même.</p>
     </div>
     <div class="tldr mt-5">
       <h2 class="text-5xl font-bold">TL;DR</h2>
-      <div class="grid grid-cols-2 gap-4 p-3 w-2/3 m-auto">
-        <div class="text-center bg-fuchsia-700 text-white text-xl h-10 flex items-center justify-center rounded-sm">Passionné</div>
-        <div class="text-center bg-fuchsia-700 text-white text-xl h-10 flex items-center justify-center rounded-sm">Écologie</div>
-        <div class="text-center bg-fuchsia-700 text-white text-xl h-10 flex items-center justify-center rounded-sm">Acharné</div>
-        <div class="text-center bg-fuchsia-700 text-white text-xl h-10 flex items-center justify-center rounded-sm">Motivé</div>
-      </div> 
+      <div class="grid lg:grid-cols-2 gap-4 p-3 w-2/3 m-auto">
+        <div class="text-center bg-fuchsia-700 text-white text-xl h-10 flex items-center justify-center rounded-sm">
+          Passionné</div>
+        <div class="text-center bg-fuchsia-700 text-white text-xl h-10 flex items-center justify-center rounded-sm">
+          Écologie</div>
+        <div class="text-center bg-fuchsia-700 text-white text-xl h-10 flex items-center justify-center rounded-sm">
+          Créatif</div>
+        <div class="text-center bg-fuchsia-700 text-white text-xl h-10 flex items-center justify-center rounded-sm">Motivé
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    padding-top: 3em;
-    min-height: 100%;
-  }
+@media (min-width: 1024px) {}
 
-  .detailed-me {
-    border-radius: 3px;
-    background-color: hsl(260, 60%, 40%);
-    width: 90vw;
-    padding: 2em;
-  }
-  .tldr {
-    border-radius: 3px;
-    background-color: hsl(290, 40%, 90%);
-    width: 90vw;
-    padding: 2em;
-    color: #262428;
-  }
+.about {
+  padding-top: 3em;
+  min-height: 100%;
+}
+
+.detailed-me {
+  border-radius: 3px;
+  background-color: hsl(260, 60%, 40%);
+  width: 90vw;
+  padding: 2em;
+}
+
+.tldr {
+  border-radius: 3px;
+  background-color: hsl(290, 40%, 90%);
+  width: 90vw;
+  padding: 2em;
+  color: #262428;
 }
 </style>
